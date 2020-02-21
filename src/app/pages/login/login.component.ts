63903d576a7c671de0931f29f9b4e7d4f86af55a
liemlylac <liemlylac@gmail.com>
2020-02-21 17:57:24 +0700

    🚧 Working in process Create login page using Bootstrap style #3

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  logoImg = '/assets/img/default/bootstrap-solid.svg';
+  logoAlt = 'logo-alt-description';
 
   constructor() {
   }
