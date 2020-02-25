e59281f6000f4c4e358972852dc6bd64060d93ad
liemlylac <liemlylac@gmail.com>
2020-02-25 16:23:29 +0700

    :construction: Working in process Create login page using Bootstrap style #3

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,42 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { UserLoginModel } from './user-login.model';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  logoImg = '/assets/img/default/bootstrap-solid.svg';
-  logoAlt = 'logo-alt-description';
 
-  constructor() {
+  errors: string[];
+  messages: string[];
+  user: UserLoginModel;
+  submitted = false;
+  rememberMe = false;
+
+  constructor(
+    private titleService: Title,
+    private router: Router,
+    ) {
   }
 
   ngOnInit() {
+    this.titleService.setTitle('Login');
+    this.user = new UserLoginModel();
   }
 
+  login(): void {
+    this.errors = [];
+    this.messages = [];
+    this.submitted = true;
+  }
+
+  gotoForgotPassword() {
+    this.router.navigate(['auth', 'forgot-password']);
+  }
+
+  gotoRegister() {
+    this.router.navigate(['auth', 'register']);
+  }
 }
